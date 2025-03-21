694d4f11cdd079abba1e4c6b4a9515248e990753
AUGUSTRMZ <a01712439@tec.mx>
2025-03-21 14:16:04 -0600

    Update PEP8 and Flake8 coding standarts

--- pacman.py
+++ pacman.py
@@ -10,7 +10,9 @@ Exercises
 """
 
 from random import choice
-from turtle import *
+from turtle import Turtle, bgcolor, clear, up, goto
+from turtle import tracer, listen, onkey, done
+from turtle import dot, update, ontimer, setup, hideturtle
 
 from freegames import floor, vector
 
